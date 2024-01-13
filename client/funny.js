6eeede31d39cd9d809044bede9cc00e88336b147
Glitch (glitch-blank-website) <none>
2024-01-13 15:45:06 +0000

    😘😋 Checkpoint ./client/funny.js:51974198/218

---
+++ client/funny.js
@@ -0,0 +1,4 @@
+// Fuck you
+function amongus() {
+writeChar("A", 1);writeChar("M", 1);writeChar("O", 1);writeChar("N", 1);writeChar("G", 2);writeChar("U", 1);writeChar("S", 1);writeChar("!", 1);writeChar("!", 1);writeChar("!", 2);
+ }
