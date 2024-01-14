518fc4909d448aae2e9eb11bad5b40d0a372734b
Glitch (glitch-blank-website) <none>
2024-01-14 06:28:05 +0000

    🙉🚝 Checkpoint ./client/warning.js:51974198/591 ./server.js:51974198/45

--- client/warning.js
+++ client/warning.js
@@ -1,6 +1,6 @@
 console.warn("%cSTOP!", "color: red; font-size: 40px;");
 console.log("%cDo not paste anything here unless you know what you're doing, Use common sense.", "color: red; font-size: 20px;");
-console.log("%cOfficial walls:")
-console.log("%c/rules: example.com", "font-size: 16px; color: blue; text-decoration: underline;");
-console.log("%c/feedback: example.com", "font-size: 16px; color: blue; text-decoration: underline;");
-console.log("%c/scripting: example.com", "font-size: 16px; color: blue; text-decoration: underline;");
+console.log("%cOfficial walls:", "font-size: 30px");
+console.log("%chttps://dimkatextwall.glitch.me/rules", "font-size: 18px");
+console.log("%chttps://dimkatextwall.glitch.me/feedback", "font-size: 18px");
+console.log("%chttps://dimkatextwall.glitch.me/scripting", "font-size: 18px");
