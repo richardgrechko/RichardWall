57c515206a184c9a8bd67be1b2ae21ce68c23af9
Glitch (glitch-blank-website) <none>
2024-01-14 06:16:05 +0000

    🌆🖐 Checkpoint ./client/warning.js:51974198/4243

--- client/warning.js
+++ client/warning.js
@@ -1,2 +1,6 @@
 console.warn("%cSTOP!", "color: red; font-size: 40px;");
 console.log("%cDo not paste anything here unless you know what you're doing, Use common sense.", "color: red; font-size: 20px;");
+console.log("%cOfficial walls:")
+console.log("%c/rules: example.com", "font-size: 16px; color: blue; text-decoration: underline;");
+console.log("%c/feedback: example.com", "font-size: 16px; color: blue; text-decoration: underline;");
+console.log("%c/scripting: example.com", "font-size: 16px; color: blue; text-decoration: underline;");
