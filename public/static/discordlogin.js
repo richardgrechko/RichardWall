60434d9c828a73e05901e3cbbe7a7f4a826f87d2
Glitch (glitch-blank-website) <none>
2024-01-29 15:04:05 +0000

    🚤🐄 Checkpoint ./public/authorized.html:51548543/75 ./public/static/discordlogin.js:51548543/546

---
+++ public/static/discordlogin.js
@@ -0,0 +1,11 @@
+//wip
+function discordLogin() {
+  var login = window.open("https://discord.com/api/oauth2/authorize?client_id=1201515886683619368&response_type=code&redirect_uri=https%3A%2F%2Fdimkatextwall.glitch.me%2Fauthorized.html&scope=identify");
+  var interval = setInterval(() => login.postMessage("", "https://dimkatextwall.glitch.me"), 500);
+  window.addEventListener("message", e => {
+    if (!e.data.code) return;
+    clearInterval(interval);
+    login.close();
+    console.log(e.data.code);
+  }, {once: true});
+}
