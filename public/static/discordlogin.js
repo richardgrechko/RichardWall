df1c7170829bc122abf71bc233d7c6523be119e7
Glitch (glitch-blank-website) <none>
2024-02-01 06:52:58 +0000

    📟🎯 Checkpoint ./public/static/discordlogin.js:51548543/3829

--- public/static/discordlogin.js
+++ public/static/discordlogin.js
@@ -1,15 +1,16 @@
 //wip
+var login;
+var interval;
+var code;
+
 function discordLogin() {
-  var login = window.open(
+  login = window.open(
     "https://discord.com/api/oauth2/authorize?client_id=1201515886683619368&response_type=code&redirect_uri=https%3A%2F%2Fdimkatextwall.glitch.me%2Fauthorized.html&scope=identify"
   );
-  var interval = setInterval(
-    () => {
-      login.postMessage("", "https://dimkatextwall.glitch.me");
-      if (login.closed) cancelDiscordLogin();
-    },
-    500
-  );
+  interval = setInterval(() => {
+    login.postMessage("", "https://dimkatextwall.glitch.me");
+    if (login.closed) cancelDiscordLogin();
+  }, 500);
   discordloginbtn.disabled = true;
   document.getElementById("login").style.display = "none";
   window.addEventListener(
@@ -17,24 +18,32 @@ function discordLogin() {
     (e) => {
       if (e.data.error) {
         showAlert("An error occurred. Please try again.", 5e3);
-        console.log("Discord login error:", e.data.error, e.data.error_description);
+        console.log(
+          "Discord login error:",
+          e.data.error,
+          e.data.error_description
+        );
         cancelDiscordLogin();
       }
       if (!e.data.code) return;
       clearInterval(interval);
       login.close();
+      code = e.data.code;
       client.sendWsMessage({ discordlogin: [e.data.code] });
     },
     { once: true }
   );
-  login.onclose = cancelDiscordLogin;
-  function cancelDiscordLogin() {
-    clearInterval(interval);
-    login.close();
-    discordloginbtn.disabled = false;
-    document.getElementById("login").style.display = "block";
-    document.getElementById("discordlogin").style.display = "none";
-  }
+  
+}
+canceldiscordlogin.onclick = cancelDiscordLogin;
+function cancelDiscordLogin() {
+  clearInterval(interval);
+  login.close();
+  discordloginbtn.disabled = false;
+  document.getElementById("login").style.display = "block";
+  document.getElementById("discordlogin").style.display = "none";
 }
 
-
+client.on("wsmessage", e => {
+  if (e.discordnoaccount) document.getElementById("discordlogin").style.display = "block";
+});
