962a4a598ddf9c285c1443a2c0b2f0e3504326dd
Glitch (glitch-blank-website) <none>
2024-02-01 06:08:00 +0000

    👜🍁 Checkpoint ./public/static/discordlogin.js:51548543/3599

--- public/static/discordlogin.js
+++ public/static/discordlogin.js
@@ -1,17 +1,37 @@
 //wip
 function discordLogin() {
-  var login = window.open("https://discord.com/api/oauth2/authorize?client_id=1201515886683619368&response_type=code&redirect_uri=https%3A%2F%2Fdimkatextwall.glitch.me%2Fauthorized.html&scope=identify");
-  var interval = setInterval(() => login.postMessage("", "https://dimkatextwall.glitch.me"), 500);
-  window.addEventListener("message", e => {
-    if (e.data.error) {
-      showAlert("An error occurred. Please try again.", 5e3);
-      console.log("Discord login error:", e.data.error, e.data.error_description);
+  var login = window.open(
+    "https://discord.com/api/oauth2/authorize?client_id=1201515886683619368&response_type=code&redirect_uri=https%3A%2F%2Fdimkatextwall.glitch.me%2Fauthorized.html&scope=identify"
+  );
+  var interval = setInterval(
+    () => {
+      login.postMessage("", "https://dimkatextwall.glitch.me");
+      if (login.closed) cancelLogin();
+    },
+    500
+  );
+  discordloginbtn.disabled = true;
+  window.addEventListener(
+    "message",
+    (e) => {
+      if (e.data.error) {
+        showAlert("An error occurred. Please try again.", 5e3);
+        console.log("Discord login error:", e.data.error, e.data.error_description);
+        cancelLogin();
+      }
+      if (!e.data.code) return;
       clearInterval(interval);
       login.close();
-    }
-    if (!e.data.code) return;
+      client.sendWsMessage({ discordlogin: [e.data.code] });
+    },
+    { once: true }
+  );
+  login.onclose = cancelLogin;
+  function cancelLogin() {
     clearInterval(interval);
     login.close();
-    client.sendWsMessage({discordlogin: [e.data.code]});
-  }, { once: true });
+    discordloginbtn.disabled = false;
+  }
 }
+
+
