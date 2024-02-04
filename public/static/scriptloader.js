db26349ef9ea41a14f3888fe5963689b915d9803
Glitch (dimkatextwall) <none>
2024-02-04 04:08:15 +0000

    💾👩‍❤️‍💋‍👩 Checkpoint ./public/static/scriptloader.js:51548543/434 ./index.html:51548543/60 ./public/static/oneko.js:51548543/30 ./public/static/textwall.js:51548543/192

--- public/static/scriptloader.js
+++ public/static/scriptloader.js
@@ -1,1 +1,10 @@
-le
+var wallName = location.pathname.replace(/^\/|\/$/g, "");
+function loadScript(url, callback) {
+  var elem = document.createElement("script");
+  elem.src = url;
+  document.head.appendChild(elem);
+  elem.onload = callback;
+}
+if (wallName == "cat") {
+  loadScript("/static/oneko.js");
+}
