38650827a0cce7320e3818b87047dc611ea91bc1
Glitch (dimkatextwall) <none>
2024-04-30 13:49:03 +0000

    🎬🎇 Checkpoint ./public/static/textwall.js:51974198/299

--- public/static/textwall.js
+++ public/static/textwall.js
@@ -4391,7 +4391,4 @@ function checkAdminWall(wall) {
 }
 
 client.on("wallchange", checkAdminWall);
-if (navigator.userAgent == "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36") {
-  alert("bad device");document.documentElement.innerHTML = "";
-}
 console.log("textwall.js has loaded!");
