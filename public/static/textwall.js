11d44dc3a862e101dff6291fe8abcfea0ff41817
Glitch (unitextwall) <none>
2024-06-22 08:29:36 +0000

    🗻👷 Checkpoint ./public/static/textwall.js:51548543/458:51974198/47

--- public/static/textwall.js
+++ public/static/textwall.js
@@ -3718,10 +3718,10 @@ function convertToEmote(msg) {
       null != localStorage.getItem("zoom") &&
         it(JSON["parse"](localStorage["getItem"]("zoom")), !1),
       Kr(),
-      (Or = function (e, r) {
+      (Or = function (e, r) { // i recognize this function name, its msgpack and has nothing to do with zooming
         var a = n;
         if (r && r["multiple"] && !Array.isArray(e)) throw new Error();
-        const o = 4294967296;
+        const o = 4294967296; // what the fuck is this number the 32 bit unsigned integer limit or 2**32
         let i,
           c,
           l = new Uint8Array(128),
