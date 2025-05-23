abbdeb6debd06314021b4525e17ec0b201953d3b
RichardGrechko736 <162537734+richardgrechko@users.noreply.github.com>
2025-05-23 11:20:01 +0500

    Add files via upload

---
+++ public/warning.js
@@ -0,0 +1,10 @@
+console.warn("%cSTOP!!!", "color: red; font-size: 50px; text-decoration: bold;");
+console.log(
+  "%cDo not paste anything here unless you know what you're doing, Use common sense.",
+  "color: red; font-size: 20px;"
+);
+console.log("%cOfficial walls:", "font-size: 30px");
+console.log("%chttps://dimkatextwall.glitch.me/rules", "font-size: 18px");
+console.log("%chttps://dimkatextwall.glitch.me/feedback", "font-size: 18px");
+console.log("%chttps://dimkatextwall.glitch.me/scripting", "font-size: 18px");
+console.log("%chttps://dimkatextwall.glitch.me/help", "font-size: 18px");
