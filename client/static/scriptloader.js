edcc1e861c7f2debb1f2a7112270f362c2f066fb
Richard Does Stuff <162537734+richardgrechko@users.noreply.github.com>
2026-03-23 14:14:43 +0000

    a

--- client/static/scriptloader.js
+++
@@ -1,8 +0,0 @@
-var wallName = getWallName();
-function loadScript(url, callback) {
-  var elem = document.createElement("script");
-  elem.src = url;
-  document.head.appendChild(elem);
-  elem.onload = callback;
-}
-console.log("loaded scripts for walls")
